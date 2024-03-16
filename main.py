2d45b827f1c3f532574686d9e5519b15476cf8a1
Byungchan <83104885+soft0725@users.noreply.github.com>
2024-03-16 10:40:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,69 @@
 import streamlit as st
 import yaml
+import time
 
 st.set_page_config(layout="wide")
 
-# YAML 파일을 로드합니다.
 with open("position.yaml", "r") as file:
     data = yaml.safe_load(file)
 
-# sidebar ---------------------------------------
 
-st.sidebar.markdown("<h1 style='text-align: center; color: black;'>이동할 경로를 클릭해주세요.</h1>", unsafe_allow_html=True)
-st.sidebar.markdown("")
-st.sidebar.markdown("")
+def same_position():
+    pass
 
-if st.sidebar.button("도서관으로 이동하기"):
-    print(f"x 값: {data['도서관']['x']}")
-    print(f"y 값: {data['도서관']['y']}")
-    print(f"z 값: {data['도서관']['z']}")
-    st.experimental_rerun()
+def move_to_goal():
+    pass
 
-if st.sidebar.button("헬스장으로 이동하기"):
-    print(f"x 값: {data['임베실']['x']}")
-    print(f"y 값: {data['임베실']['y']}")
-    print(f"z 값: {data['임베실']['z']}")
-    st.experimental_rerun()
+def button_clicked(key):
+    x, y, z = data[key].values()
+    print(x, y, z)
+    show_message()
 
-# mid ----------------------------------------------
+def show_message():
+    pass
 
-col1, col2 = st.columns([1, 1])  # 2개의 열을 생성합니다.
-col3 = st.empty()  # col3는 비어 있는 상태로 생성합니다.
 
-with col1:
-    st.title('here is column1')
+def main():
+    st.markdown("""
+    <h1 style='text-align: center; color: black;'>이동할 경로를 클릭해주세요.</h1><br><br><br><br>
+    """, unsafe_allow_html=True)
 
-with col2:
-    st.title('here is column2')
-    st.checkbox('this is checkbox1 in col2 ')
+    columns_1 = st.columns((3, 3, 3, 3))
+    columns_2 = st.columns((1, 10, 1))  # layer 크기 지정
 
-# '현재 상황'의 타이틀을 넣습니다.
-st.title('현재 상황')
 
-# 큰 네모 박스를 생성하여 텍스트를 넣습니다.
-st.success('작업이 성공했을때 사용하자')
+    with columns_1[0]:
+        if st.button("임베디드 사무실로 이동하기", key="embedlab", use_container_width=True):
+            button_clicked("embedlab")
+        if st.button("SW개발실 3으로 이동하기", key="class3", use_container_width=True):
+            button_clicked("class3")
+
+    with columns_1[1]:
+        if st.button("헬스장으로 이동하기", key="gym", use_container_width=True):
+            button_clicked("gym")
+        if st.button("SW개발실 2으로 이동하기", key="class2", use_container_width=True):
+            button_clicked("class2")
+
+    with columns_1[2]:
+        if st.button("작은 발표장으로 이동하기", key="smallpresroom", use_container_width=True):
+            button_clicked("smallpresroom")
+        if st.button("SW개발실 1으로 이동하기", key="class1", use_container_width=True):
+            button_clicked("class1")
+
+    with columns_1[3]:
+        if st.button("도서관으로 이동하기", key="library", use_container_width=True):
+            button_clicked("library")
+        if st.button("화장실로 이동하기", key="toilet", use_container_width=True):
+            button_clicked("toilet")
+
+    st.write("")
+
+    with columns_2[1]:
+        value = "이동합니다."
+        st.markdown(f"""
+           <h4 style='text-align: center; color: black;'><br><br><br><br>{value}</h4>
+           """, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
+
